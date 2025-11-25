5281fe5863814f035059e232c28c57695ef05ddf
laurent bernabé <laurent.bernabe@gmail.com>
2025-11-25 21:16:07 +0100

    feat: function addMove() for history component

--- src/components/MovesHistory.tsx
+++ src/components/MovesHistory.tsx
@@ -1,23 +1,49 @@
 import "./MovesHistory.css";
 import MoveHistoryNode, { MoveHistoryNodeProps } from "./MoveHistoryNode";
 import { Fragment } from "react/jsx-runtime";
-import { useState } from "react";
-import { useInterval } from "ahooks";
+import { useEffect, useState } from "react";
+import { useUpdateEffect } from "ahooks";
+
+const sanConversions = {
+  white: {
+    N: "♘",
+    B: "♗",
+    R: "♖",
+    Q: "♕",
+    K: "♔",
+  },
+  black: {
+    N: "♞",
+    B: "♝",
+    R: "♜",
+    Q: "♛",
+    K: "♚",
+  },
+};
+
+const pieceRegex = /[NBRQK]/;
+
+function convertSanToFan(san: string, isWhiteMove: boolean) {
+  const colorIndex = isWhiteMove ? "white" : "black";
+  return san.replace(pieceRegex, (match) => {
+    const innerMap = sanConversions[colorIndex];
+    return innerMap[match as keyof typeof innerMap];
+  });
+}
 
 function MovesHistory() {
   const [moves, setMoves] = useState<Array<MoveHistoryNodeProps>>([]);
 
-  function addSampleMove() {
+  function addMove(moveSan: string, isWhiteMove: boolean) {
+    const moveCaption = convertSanToFan(moveSan, isWhiteMove);
     const newMove = {
-      fan: "♝f6",
+      fan: moveCaption,
       fen: "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2",
       clickCallback: (fen: string) => console.log(`Got fen ${fen}.`),
     };
 
     setMoves((oldMoves) => [...oldMoves, newMove]);
   }
-
-  useInterval(() => addSampleMove(), 50);
 
   return (
     <div className="history">
