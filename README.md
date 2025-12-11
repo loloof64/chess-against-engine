fdc07b0065550bc88fc38c67bae32ffbc1557717
laurent bernabé <laurent.bernabe@gmail.com>
2025-12-11 17:25:44 +0100

    feat: use images for toolbar buttons
    instead of text

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ Play chess against your device
 ### Fonts
 
 Font Free Serif downloaded from https://fonts2u.com/download/free-serif.font.
+
+### Icons
+
+Some icons have been downloaded from [SVGRepo](https://www.svgrepo.com) :
+
+- start.svg : https://www.svgrepo.com/svg/422652/chess-game-sport
+- stop.svg : https://www.svgrepo.com/svg/501832/stop
+- reverse.svg : https://www.svgrepo.com/svg/414907/arrows-vertical-direction
--- src/components/toolbar/Toolbar.tsx
+++ src/components/toolbar/Toolbar.tsx
@@ -1,19 +1,16 @@
 import "./Toolbar.css";
 import { GameActionType, useGameDispatch } from "../../stores/game/GameContext";
 
+import StartGameImg from "./icons/start.svg";
+import StopGameImg from "./icons/stop.svg";
+import ReverseBoardImg from "./icons/reverse.svg";
+
 function Toolbar() {
   const dispatch = useGameDispatch();
 
   function startNewDefaultGame() {
     dispatch({
       type: GameActionType.startNewDefaultGame,
-    });
-  }
-
-  function startNewCustomGame() {
-    dispatch({
-      type: GameActionType.startNewGame,
-      value: "r7/1P6/8/7K/k7/8/8/1Q6 w - - 0 1",
     });
   }
 
@@ -31,10 +28,15 @@ function Toolbar() {
 
   return (
     <div className="toolbar">
-      <button onClick={startNewDefaultGame}>New game</button>
-      <button onClick={startNewCustomGame}>New custom game</button>
-      <button onClick={stopGame}>Stop game</button>
-      <button onClick={reverseBoard}>Reverse board</button>
+      <button onClick={startNewDefaultGame}>
+        <img src={StartGameImg}></img>
+      </button>
+      <button onClick={stopGame}>
+        <img src={StopGameImg}></img>
+      </button>
+      <button onClick={reverseBoard}>
+        <img src={ReverseBoardImg}></img>
+      </button>
     </div>
   );
 }
