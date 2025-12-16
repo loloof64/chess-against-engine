527edcc0b360e7ebf92868fef29886b5de11e70d
laurent bernabé <laurent.bernabe@gmail.com>
2025-12-16 19:27:26 +0100

    feat: implement advanced editor options

--- src/components/position_editor/AdvancedOptions.tsx
+++ src/components/position_editor/AdvancedOptions.tsx
@@ -1,7 +1,156 @@
 import { t } from "i18next";
 import "./AdvancedOptions.css";
+import {
+  PositionEditorActionType,
+  usePositionEditor,
+  usePositionEditorDispatch,
+} from "../../stores/game/PositionEditorContext";
+import { Chess, Color } from "chess.js";
 
 function AdvancedOptions() {
+  const { currentPosition } = usePositionEditor();
+  const dispatch = usePositionEditorDispatch();
+
+  function handleChangeTurn(newColor: Color) {
+    let gameLogic = new Chess(currentPosition);
+    gameLogic.setTurn(newColor);
+    dispatch({
+      type: PositionEditorActionType.changeCurrentPosition,
+      value: gameLogic.fen(),
+    });
+  }
+
+  function handleCastleChange(type: string, isToBeAdded: boolean) {
+    let positionParts = currentPosition.split(" ");
+    let castlePart = positionParts[2];
+    if (isToBeAdded) {
+      if (!castlePart.includes(type)) {
+        castlePart += type;
+        castlePart = adaptCastles(castlePart);
+      }
+    } else {
+      if (castlePart.includes(type)) {
+        castlePart = castlePart
+          .split("")
+          .filter((e) => e !== type)
+          .join("");
+        castlePart = adaptCastles(castlePart);
+      }
+    }
+    positionParts[2] = castlePart;
+    const newPosition = positionParts.join(" ");
+    try {
+      new Chess(newPosition); // check validity
+      dispatch({
+        type: PositionEditorActionType.changeCurrentPosition,
+        value: newPosition,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+    //TODO check for validity
+  }
+
+  function handleEnPassantFileChange(value: string) {
+    let positionParts = currentPosition.split(" ");
+    const whiteTurn = currentPosition.split(" ")[1] === "w";
+    const rank = whiteTurn ? "6" : "3";
+    const enPassant = value === "-" ? value : `${value}${rank}`;
+    positionParts[3] = enPassant;
+    const newPosition = positionParts.join(" ");
+    try {
+      new Chess(newPosition); // check validity
+      dispatch({
+        type: PositionEditorActionType.changeCurrentPosition,
+        value: newPosition,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+    //TODO check for validity
+  }
+
+  function handleDrawHalfMovesCountChange(newValue: string) {
+    const newValueAsNum = parseInt(newValue);
+    if (isNaN(newValueAsNum) || newValueAsNum < 0) {
+      console.error("Invalid number for half moves count");
+      return;
+    }
+
+    let positionParts = currentPosition.split(" ");
+    positionParts[4] = newValueAsNum.toString();
+    const newPosition = positionParts.join(" ");
+
+    dispatch({
+      type: PositionEditorActionType.changeCurrentPosition,
+      value: newPosition,
+    });
+  }
+
+  function handleMoveNumberChange(newValue: string) {
+    const newValueAsNum = parseInt(newValue);
+    if (isNaN(newValueAsNum) || newValueAsNum < 1) {
+      console.error("Invalid move number");
+      return;
+    }
+
+    let positionParts = currentPosition.split(" ");
+    positionParts[5] = newValueAsNum.toString();
+    const newPosition = positionParts.join(" ");
+
+    dispatch({
+      type: PositionEditorActionType.changeCurrentPosition,
+      value: newPosition,
+    });
+  }
+
+  function adaptCastles(value: string): string {
+    /*
+    Filter and order letters :
+    1) should be letters "K"
+    2) should be letters "Q"
+    3) should be letters "k"
+    4) should be letters "q"
+
+    Then if result is empty, return "-"
+    otherwise the result
+    */
+
+    const allowed = ["K", "Q", "k", "q"];
+    let filtered = value
+      .split("")
+      .filter((c) => allowed.includes(c))
+      .filter((c, i, arr) => arr.indexOf(c) === i); // remove duplicates
+
+    let ordered = ["K", "Q", "k", "q"]
+      .filter((c) => filtered.includes(c))
+      .join("");
+    return ordered.length === 0 ? "-" : ordered;
+  }
+
+  function getCurrentTurn(): Color {
+    return currentPosition.split(" ")[1] as Color;
+  }
+
+  function getCurrentCastles(): Array<string> {
+    //TODO check for validity
+    return currentPosition.split(" ")[2].split("");
+  }
+
+  function getCurrentEnPassant(): string {
+    //TODO check for validity
+    const enPassantPart = currentPosition.split(" ")[3];
+    return enPassantPart === "-" ? enPassantPart : enPassantPart.charAt(0);
+  }
+
+  function getCurrentDrawHalfMovesCount(): string {
+    return currentPosition.split(" ")[4];
+  }
+
+  function getCurrentMoveNumber(): string {
+    return currentPosition.split(" ")[5];
+  }
+
   return (
     <div className="advancedOptions">
       <div className="field">
@@ -12,8 +161,8 @@ function AdvancedOptions() {
             name="turn"
             value="whiteTurn"
             id="whiteTurn"
-            checked
-            onChange={() => {}}
+            onChange={() => handleChangeTurn("w")}
+            checked={getCurrentTurn() === "w"}
           />
           <label htmlFor="whiteTurn">
             {t("dialogs.positionEditor.advanced.turn.white")}
@@ -25,7 +174,8 @@ function AdvancedOptions() {
             name="turn"
             value="blackTurn"
             id="blackTurn"
-            onChange={() => {}}
+            onChange={() => handleChangeTurn("b")}
+            checked={getCurrentTurn() === "b"}
           />
           <label htmlFor="blackTurn">
             {t("dialogs.positionEditor.advanced.turn.black")}
@@ -39,7 +189,11 @@ function AdvancedOptions() {
             type="checkbox"
             name="castleWhiteOO"
             id="castleWhiteOO"
-            onChange={() => {}}
+            onChange={(e) => {
+              const isChecked = e.target.checked;
+              handleCastleChange("K", isChecked);
+            }}
+            checked={getCurrentCastles().includes("K")}
           />
           <label htmlFor="castleWhiteOO">
             {t("dialogs.positionEditor.advanced.castles.whiteOO")}
@@ -50,7 +204,11 @@ function AdvancedOptions() {
             type="checkbox"
             name="castleWhiteOOO"
             id="castleWhiteOOO"
-            onChange={() => {}}
+            onChange={(e) => {
+              const isChecked = e.target.checked;
+              handleCastleChange("Q", isChecked);
+            }}
+            checked={getCurrentCastles().includes("Q")}
           />
           <label htmlFor="castleWhiteOOO">
             {t("dialogs.positionEditor.advanced.castles.whiteOOO")}
@@ -61,7 +219,11 @@ function AdvancedOptions() {
             type="checkbox"
             name="castleBlackOO"
             id="castleBlackOO"
-            onChange={() => {}}
+            onChange={(e) => {
+              const isChecked = e.target.checked;
+              handleCastleChange("k", isChecked);
+            }}
+            checked={getCurrentCastles().includes("k")}
           />
           <label htmlFor="castleBlackOO">
             {t("dialogs.positionEditor.advanced.castles.blackOO")}
@@ -72,7 +234,11 @@ function AdvancedOptions() {
             type="checkbox"
             name="castleBlackOOO"
             id="castleBlackOOO"
-            onChange={() => {}}
+            onChange={(e) => {
+              const isChecked = e.target.checked;
+              handleCastleChange("q", isChecked);
+            }}
+            checked={getCurrentCastles().includes("q")}
           />
           <label htmlFor="castleBlackOOO">
             {t("dialogs.positionEditor.advanced.castles.blackOOO")}
@@ -87,11 +253,10 @@ function AdvancedOptions() {
         <select
           name="enPassantFile"
           className="enPassantFile"
-          onChange={() => {}}
+          onChange={(e) => handleEnPassantFileChange(e.target.value)}
+          defaultValue={getCurrentEnPassant()}
         >
-          <option value="none" defaultValue="-">
-            -
-          </option>
+          <option value="-">-</option>
           <option value="a">a</option>
           <option value="b">b</option>
           <option value="c">c</option>
@@ -107,14 +272,24 @@ function AdvancedOptions() {
         <label htmlFor="drawHalfMovesCount">
           {t("dialogs.positionEditor.advanced.drawHalfMovesCount")}
         </label>
-        <input type="number" name="drawHalfMovesCount" />
+        <input
+          type="number"
+          name="drawHalfMovesCount"
+          onChange={(e) => handleDrawHalfMovesCountChange(e.target.value)}
+          value={getCurrentDrawHalfMovesCount()}
+        />
       </div>
 
       <div className="field">
         <label htmlFor="moveNumber">
           {t("dialogs.positionEditor.advanced.moveNumber")}
         </label>
-        <input type="number" name="moveNumber" />
+        <input
+          type="number"
+          name="moveNumber"
+          onChange={(e) => handleMoveNumberChange(e.target.value)}
+          value={getCurrentMoveNumber()}
+        />
       </div>
     </div>
   );
