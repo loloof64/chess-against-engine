63b4751c5dccd0622025cf17b1b9c68ff8f2cd5a
laurent bernabé <laurent.bernabe@gmail.com>
2025-12-25 11:20:34 +0100

    fix: add left margin on Android in landscape

--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -4,11 +4,16 @@ import "./Game.css";
 import Board from "../board/Board";
 import useWindowOrientation from "../../utils/useWindowOrientation";
 import InlineMovesHistory from "../move_history/InlineMovesHistory";
+import getPlatformKind, { PlatformKind } from "../../utils/PlatformKind";
 
 function Game() {
   const { orientation } = useWindowOrientation();
   return orientation === "landscape" ? (
-    <div className="game">
+    <div
+      className={`game ${
+        getPlatformKind() === PlatformKind.android ? "android" : ""
+      }`}
+    >
       <div className="board">
         <Board />
       </div>
